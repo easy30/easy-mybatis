e281fd3e60b9dd59572a0ccd5d909682a112389d
maruixiang@sina.com <maruixiang@sina.com>
2023-08-17 10:51:02 +0800

    change to DefaultEntityB

--- src/main/java/com/github/easy30/easymybatis/Generation.java
+++ src/main/java/com/github/easy30/easymybatis/Generation.java
@@ -6,6 +6,14 @@ import javax.persistence.GeneratedValue;
 public interface Generation<T>
 {
 
+	 /**
+	 *
+ 	 * @param entity  实体类
+	 * @param property 实体类属性
+	 * @param table  表名
+	 * @param arg  ColumnGeneration注解上的参数
+	 * @return
+	 */
 	 T generate(Object entity,String property,String table,String arg);
 	 
 
